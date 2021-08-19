a4f35d68422ee567d005769a7d56c584782a6e40
ONEXIN <onexin@qq.com>
2021-08-19 09:09:28 +0800

    Update actually sanitize

--- soeasy/publish.portal2.php
+++ soeasy/publish.portal2.php
@@ -22,6 +22,15 @@ if(!defined('ABSPATH')) {
 //set_time_limit(60);
 ignore_user_abort();
 
+//------------------------Sanitize--------------------------------
+
+	$_POST['title'] = sanitize_text_field($_POST['title']);
+	$_POST['content'] = wp_kses($_POST['content'], 1);
+	$_POST['catid'] = sanitize_text_field($_POST['catid']);
+	$_POST['tags'] = sanitize_text_field($_POST['tags']);
+	$_POST['occurl'] = sanitize_text_field($_POST['occurl']);
+	$_POST['occsite'] = sanitize_text_field($_POST['occsite']);
+
 //------------------------TAG--------------------------------
 
 	$catid = explode(' ', $_POST['catid']);
@@ -54,11 +63,6 @@ $_POST['user_ID'] = $user_ID;
 	//}
 	
 //-----------------------------READY GO--------------------------------------------	
-        // sanitize
-		$_POST['title'] = sanitize_text_field($_POST['title']);
-		$_POST['content'] = wp_kses($_POST['content'], 1);
-		$_POST['catid'] = sanitize_text_field($_POST['catid']);
-		$_POST['tags'] = sanitize_text_field($_POST['tags']);
         	
 		// <!--nextpage-->
 		$_POST['content'] = preg_replace("/\<hr\>$/", '', $_POST['content']);	
@@ -103,7 +107,6 @@ function onexin_bigdta_censor($content){
 
 	if($_OBD['filter_content']){
 		$content = onexin_bigdata_filter($content, $_OBD['filter_content']);
-		//$_POST['content'] = str_replace(explode('|', $_OBD['filter_content']), '', $_POST['content']);
 	}
 	return $content;
 }
